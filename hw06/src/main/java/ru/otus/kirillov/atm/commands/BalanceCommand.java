9c19700e06b3e34b000665fac49c455a0094d84b
kirillovsky <kirillow.aleks@gmail.com>
2017-12-10 12:16:30 +0300

    remove

--- hw06/src/main/java/ru/otus/kirillov/atm/commands/BalanceCommand.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.kirillov.atm.commands;
-
-/**
- * Created by Александр on 07.12.2017.
- */
-public class BalanceCommand {
-}
--- hw06/src/main/java/ru/otus/kirillov/atm/commands/queries/UndoToDefaultQuery.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.kirillov.atm.commands.queries;
-
-/**
- * Created by Александр on 07.12.2017.
- */
-public class UndoToDefaultQuery {
-}
