9d4a5bafa379100b784782b918728d1ff50a92ad
kirillovsky <kirillow.aleks@gmail.com>
2018-01-14 12:01:41 +0300

    Поправил TypeAdapter

--- hw08/src/main/java/ru/otus/kirillov/adapters/TypeAdapter.java
+++ hw08/src/main/java/ru/otus/kirillov/adapters/TypeAdapter.java
@@ -3,5 +3,10 @@ package ru.otus.kirillov.adapters;
 /**
  * Created by Александр on 12.01.2018.
  */
-public interface TypeAdapter<T> {
+public interface TypeAdapter{
+
+    boolean isApplicableForType(Class<?> clazz);
+
+    void apply();
+
 }
