a42d970a7a9178ac55c6e0b3c4018113df02fdd2
kirillovsky <kirillow.aleks@gmail.com>
2018-01-14 12:00:59 +0300

    Добавил методы requiredNotNull

--- hw08/src/main/java/ru/otus/kirillov/utils/CommonUtils.java
+++ hw08/src/main/java/ru/otus/kirillov/utils/CommonUtils.java
@@ -1,7 +1,22 @@
 package ru.otus.kirillov.utils;
 
-/**
+import java.util.Arrays;
+
+/** Утилиты обзего назначения
  * Created by Александр on 14.01.2018.
  */
-public class CommonUtils {
+public final class CommonUtils {
+
+    private CommonUtils() {
+    }
+
+    public static void requiredNotNull(Object o) {
+        if(o == null) {
+            throw new IllegalArgumentException("Required must be not null!");
+        }
+    }
+
+    public static void requiredNotNull(Object... o) {
+        Arrays.stream(o).forEach(CommonUtils::requiredNotNull);
+    }
 }
