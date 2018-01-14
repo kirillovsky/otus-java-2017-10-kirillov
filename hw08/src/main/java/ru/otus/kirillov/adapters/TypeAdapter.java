a980413bdfdb60d404360624212055518267f1c5
kirillovsky <kirillow.aleks@gmail.com>
2018-01-14 12:12:19 +0300

    Поправил форматирование

--- hw08/src/main/java/ru/otus/kirillov/adapters/TypeAdapter.java
+++ hw08/src/main/java/ru/otus/kirillov/adapters/TypeAdapter.java
@@ -1,9 +1,10 @@
 package ru.otus.kirillov.adapters;
 
-/** Интерфейс адаптеров для сериализации объектов определенных типов.
+/**
+ * Интерфейс адаптеров для сериализации объектов определенных типов.
  * Created by Александр on 12.01.2018.
  */
-public interface TypeAdapter{
+public interface TypeAdapter {
 
     boolean isApplicableForType(Class<?> clazz);
 
