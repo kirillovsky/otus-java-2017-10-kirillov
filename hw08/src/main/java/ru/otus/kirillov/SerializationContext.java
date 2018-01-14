537def34363a6c45c57f3b2f89fc3a7b2f94cf2d
kirillovsky <kirillow.aleks@gmail.com>
2018-01-14 12:03:40 +0300

    доработал SerializationContext

--- hw08/src/main/java/ru/otus/kirillov/SerializationContext.java
+++ hw08/src/main/java/ru/otus/kirillov/SerializationContext.java
@@ -1,7 +1,41 @@
 package ru.otus.kirillov;
 
-/**
+import ru.otus.kirillov.adapters.TypeAdapter;
+import ru.otus.kirillov.utils.CommonUtils;
+
+import javax.json.stream.JsonGenerator;
+import java.util.Collections;
+import java.util.List;
+
+/** Контекст сериализации. Используется в процессе сериализации объекта.
  * Created by Александр on 12.01.2018.
  */
-public class SerializationContext {
+public final class SerializationContext {
+
+    private final Object value;
+
+    private final JsonGenerator generator;
+
+    private final List<TypeAdapter> adapters;
+
+    public static SerializationContext of(Object value, JsonGenerator generator, List<TypeAdapter> adapters) {
+        CommonUtils.requiredNotNull(value, generator, adapters);
+        return new SerializationContext(value, generator, adapters);
+    }
+
+    public SerializationContext(Object value, JsonGenerator generator, List<TypeAdapter> adapters) {
+        this.value = value;
+        this.generator = generator;
+        this.adapters = Collections.unmodifiableList(adapters);
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public JsonGenerator getGenerator() {
+        return generator;
+    }
+
+
 }
