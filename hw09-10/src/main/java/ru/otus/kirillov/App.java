e24f9fa553b0a795a41c08ad184e97d12e861e41
kirillovsky <kirillow.aleks@gmail.com>
2018-01-19 19:42:57 +0300

    Initial commit

---
+++ hw09-10/src/main/java/ru/otus/kirillov/App.java
@@ -0,0 +1,12 @@
+package ru.otus.kirillov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
