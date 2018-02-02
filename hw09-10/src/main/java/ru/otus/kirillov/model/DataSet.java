26ac5dd7baf6ae22afc9705756355d665d99cd32
kirillovsky <kirillow.aleks@gmail.com>
2018-02-02 19:23:35 +0300

    Test fixes for MyOrmFramework

--- hw09-10/src/main/java/ru/otus/kirillov/model/DataSet.java
+++ hw09-10/src/main/java/ru/otus/kirillov/model/DataSet.java
@@ -4,12 +4,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Random;
 
 /** Базовый класс для всех моделей
  * Created by Александр on 20.01.2018.
  */
 @MappedSuperclass
 public abstract class DataSet {
+
+    private static Random ID_GENERATOR =
+            new Random(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,5 +27,11 @@ public abstract class DataSet {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public DataSet() {
+        //Так как самописный ORM-фреймворк не умеет сетить
+        //Только что сгенеренную дату, будем генерить id-шники так
+        id = ID_GENERATOR.nextLong();
     }
 }
--- hw09-10/src/main/java/ru/otus/kirillov/service/factory/myorm/MyOrmDBServiceFactory.java
+++ hw09-10/src/main/java/ru/otus/kirillov/service/factory/myorm/MyOrmDBServiceFactory.java
@@ -3,25 +3,37 @@ package ru.otus.kirillov.service.factory.myorm;
 import ru.otus.kirillov.configuration.DBServiceConfig;
 import ru.otus.kirillov.dao.myorm.AbstractMyOrmDao;
 import ru.otus.kirillov.model.AddressDataSet;
+import ru.otus.kirillov.model.DataSet;
 import ru.otus.kirillov.model.PhoneDataSet;
 import ru.otus.kirillov.model.UserDataSet;
+import ru.otus.kirillov.myorm.MyOrmService;
+import ru.otus.kirillov.myorm.MyOrmServiceImpl;
 import ru.otus.kirillov.service.DBService;
 import ru.otus.kirillov.service.DBServiceFactory;
 import ru.otus.kirillov.service.factory.AbstractDBServiceFactory;
+import ru.otus.kirillov.service.myorm.DBServiceMyOrmImpl;
+import ru.otus.kirillov.utils.CommonUtils;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-/** Фабрика для создания {@link DBServiceFactory} с реализацией через
+/**
+ * Фабрика для создания {@link DBServiceFactory} с реализацией через
  * MyORM
  * Created by Александр on 28.01.2018.
  */
 public class MyOrmDBServiceFactory extends AbstractDBServiceFactory {
 
     //region DAO-классы для модели из задания
-    public static class UserDataSetDao extends AbstractMyOrmDao<UserDataSet> {}
-    public static class AddressDataSetDao extends AbstractMyOrmDao<AddressDataSet> {}
-    public static class PhoneDataSetDao extends AbstractMyOrmDao<PhoneDataSet> {}
+    public static class UserDataSetDao extends AbstractMyOrmDao<UserDataSet> {
+    }
+
+    public static class AddressDataSetDao extends AbstractMyOrmDao<AddressDataSet> {
+    }
+
+    public static class PhoneDataSetDao extends AbstractMyOrmDao<PhoneDataSet> {
+    }
     //endregion
 
     private static final List<Class<?>> DEFAULT_DAO_CLASSES =
@@ -34,6 +46,15 @@ public class MyOrmDBServiceFactory extends AbstractDBServiceFactory {
 
     @Override
     public DBService createDBService(DBServiceConfig config) {
-        return null;
+        CommonUtils.requiredNotNull(config);
+        List<Class<? extends DataSet>> entityClasses =
+                getDaoClasses(config.getUsersDaoClasses())
+                        .map(clazz -> (Class<? extends DataSet>) getDaoTypeArgument(clazz))
+                        .collect(Collectors.toList());
+
+        MyOrmService ormService = new MyOrmServiceImpl(config, entityClasses);
+        DBService dbService = new DBServiceMyOrmImpl(ormService);
+        registerDAO(dbService, config);
+        return dbService;
     }
 }
