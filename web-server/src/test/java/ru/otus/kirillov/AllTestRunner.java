663b5b2f82114fbc96a556309ce54582357ee0cb
kirillovsky <kirillow.aleks@gmail.com>
2018-03-04 19:02:10 +0300

    add all test runner

---
+++ web-server/src/test/java/ru/otus/kirillov/AllTestRunner.java
@@ -0,0 +1,23 @@
+package ru.otus.kirillov;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import ru.otus.kirillov.model.commands.CommandInvokerTest;
+import ru.otus.kirillov.model.commands.getCacheStats.GetCacheStatsCommandTest;
+import ru.otus.kirillov.model.commands.login.LoginCommandTest;
+import ru.otus.kirillov.model.commands.logout.LogoutCommandTest;
+
+import static org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+        GetCacheStatsCommandTest.class,
+        LoginCommandTest.class,
+        LogoutCommandTest.class,
+        CommandInvokerTest.class
+})
+public class AllTestRunner {
+}
