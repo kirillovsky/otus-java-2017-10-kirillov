0f69484510afcd4dc01255500e6a4c0d6f4765cc
kirillovsky <kirillow.aleks@gmail.com>
2018-03-04 22:17:10 +0300

    add db operation simulator test to test runner

--- web-server/src/test/java/ru/otus/kirillov/AllTestRunner.java
+++ web-server/src/test/java/ru/otus/kirillov/AllTestRunner.java
@@ -3,6 +3,7 @@ package ru.otus.kirillov;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import ru.otus.kirillov.model.commands.CommandInvokerTest;
+import ru.otus.kirillov.model.commands.dbOperationStub.DbWorkSimulatorTest;
 import ru.otus.kirillov.model.commands.getCacheStats.GetCacheStatsCommandTest;
 import ru.otus.kirillov.model.commands.login.LoginCommandTest;
 import ru.otus.kirillov.model.commands.logout.LogoutCommandTest;
@@ -17,7 +18,8 @@ import static org.junit.runners.Suite.SuiteClasses;
         GetCacheStatsCommandTest.class,
         LoginCommandTest.class,
         LogoutCommandTest.class,
-        CommandInvokerTest.class
+        CommandInvokerTest.class,
+        DbWorkSimulatorTest.class
 })
 public class AllTestRunner {
 }
