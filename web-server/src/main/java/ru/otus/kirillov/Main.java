43362f01c577d3ab955d5b2dcc5e3cbc1981645f
kirillovsky <kirillow.aleks@gmail.com>
2018-03-24 15:19:12 +0300

    Move server creation logic to own class

--- web-server/src/main/java/ru/otus/kirillov/Main.java
+++ web-server/src/main/java/ru/otus/kirillov/Main.java
@@ -1,38 +1,17 @@
 package ru.otus.kirillov;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import ru.otus.kirillov.model.service.dbOperationStub.DbWorkScheduledSimulator;
-import ru.otus.kirillov.model.service.dbOperationStub.DbWorkSimulator;
-
-import static ru.otus.kirillov.configuraton.WebServerConfiguration.*;
+import static ru.otus.kirillov.configuraton.WebServerConfiguration.createDbWorkSimulatorScheduler;
 
 public class Main {
 
     private final static int PORT = 8090;
-    private final static String PUBLIC_HTML = "statics";
     private static DbWorkScheduledSimulator simulator;
 
     public static void main(String[] args) throws Exception {
         initScheduledSimulator();
 
-        ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setResourceBase(PUBLIC_HTML);
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-
-        context.addServlet(new ServletHolder(createMainServlet()), "/main");
-        context.addServlet(new ServletHolder(createLoginServlet()), "/login");
-        context.addServlet(new ServletHolder(createLogoutServlet()), "/logout");
-        context.addServlet(new ServletHolder(createGetCacheStatsServlet()), "/cache-stats");
-        context.addServlet(new ServletHolder(createErrorPageServlet()), "/error");
-
-        Server server = new Server(PORT);
-        server.setHandler(new HandlerList(resourceHandler, context));
-
+        WebServer server = new WebServer(PORT);
         server.start();
         server.join();
 
---
+++ web-server/src/main/java/ru/otus/kirillov/WebServer.java
@@ -0,0 +1,55 @@
+package ru.otus.kirillov;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import static ru.otus.kirillov.configuraton.WebServerConfiguration.createErrorPageServlet;
+import static ru.otus.kirillov.configuraton.WebServerConfiguration.createGetCacheStatsServlet;
+import static ru.otus.kirillov.configuraton.WebServerConfiguration.createLoginServlet;
+import static ru.otus.kirillov.configuraton.WebServerConfiguration.createLogoutServlet;
+import static ru.otus.kirillov.configuraton.WebServerConfiguration.createMainServlet;
+
+public class WebServer {
+
+    private final static Logger log = LogManager.getLogger();
+    private final static String PUBLIC_HTML = "statics";
+    private final Server server;
+
+    public WebServer(int portNumber) {
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase(PUBLIC_HTML);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        context.addServlet(new ServletHolder(createMainServlet()), "/main");
+        context.addServlet(new ServletHolder(createLoginServlet()), "/login");
+        context.addServlet(new ServletHolder(createLogoutServlet()), "/logout");
+        context.addServlet(new ServletHolder(createGetCacheStatsServlet()), "/cache-stats");
+        context.addServlet(new ServletHolder(createErrorPageServlet()), "/error");
+
+        server = new Server(portNumber);
+        server.setHandler(new HandlerList(resourceHandler, context));
+    }
+
+    public void start() throws Exception {
+        server.start();
+        log.info("Server started!!!");
+    }
+
+    public void join() throws InterruptedException {
+        server.join();
+    }
+
+    public void safelyStop() {
+        try {
+            server.stop();
+        } catch (Exception e) {
+            log.error("Faulty server stops!");
+            log.catching(e);
+        }
+    }
+}
