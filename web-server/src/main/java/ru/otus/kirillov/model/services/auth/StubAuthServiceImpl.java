575b0ca118908e5ade3830ef8558fa0d60b35e05
kirillovsky <kirillow.aleks@gmail.com>
2018-04-01 22:05:28 +0300

    implement multisession Auth service

--- web-server/src/main/java/ru/otus/kirillov/model/services/auth/StubAuthServiceImpl.java
+++ web-server/src/main/java/ru/otus/kirillov/model/services/auth/StubAuthServiceImpl.java
@@ -1,15 +1,23 @@
 package ru.otus.kirillov.model.services.auth;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.otus.kirillov.utils.AESSecurity;
 import ru.otus.kirillov.utils.CommonUtils;
 
+import java.util.Collections;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Заглушка для сервиса авторизации и авторизации
  */
 public class StubAuthServiceImpl implements AuthService {
+
+    private static final Logger log = LogManager.getLogger();
 
     public static class NotAuthorizedException extends RuntimeException {
         public NotAuthorizedException(String userName, String password) {
@@ -23,7 +31,7 @@ public class StubAuthServiceImpl implements AuthService {
 
     private final String password;
 
-    private String currentSessionId = null;
+    private Set<String> activeSessions = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
     public StubAuthServiceImpl(AESSecurity security) {
         this.security = CommonUtils.retunIfNotNull(security);
@@ -41,20 +49,25 @@ public class StubAuthServiceImpl implements AuthService {
     @Override
     public String login(String userName, String password) {
         if (!Objects.equals(username, userName) || !Objects.equals(this.password, password)) {
+            log.error("Username = {}, password = {} not authorized", userName, password);
             throw new NotAuthorizedException(userName, password);
         }
-        return currentSessionId = UUID.randomUUID().toString();
+        String sessionId = UUID.randomUUID().toString();
+        activeSessions.add(sessionId);
+        log.info("Username = {}, password = {} authorization success. Session = {}", userName, password, sessionId);
+        return sessionId;
     }
 
     @Override
     public void logout(String sessionId, String userName) {
         CommonUtils.requiredNotNull(sessionId, userName);
         if (isSessionForUserExists(sessionId, userName)) {
-            currentSessionId = null;
+            activeSessions.remove(sessionId);
+            log.info("Username = {}, Session = {}. Logout", userName, sessionId);
         }
     }
 
     private boolean isSessionForUserExists(String sessionId, String userName) {
-        return Objects.equals(currentSessionId, sessionId) && Objects.equals(username, userName);
+        return activeSessions.contains(sessionId) && Objects.equals(username, userName);
     }
 }
